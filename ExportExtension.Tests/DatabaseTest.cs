506000acb59a948056b9906133db77ac8c8f5af5
Seaw.Jean.Aw <Twitter: @OOPath>
2013-12-08 02:20:49 +0700

    Add README.md

---
+++ ExportExtension.Tests/DatabaseTest.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: OOPath
+ * Date: 12/8/2013
+ * Time: 2:01 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace ExportExtension.Tests
+{
+	[TestFixture]
+	public class DatabaseTest
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+	}
+}
---
+++ ExportExtension/EnumSource.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: OOPath
+ * Date: 12/8/2013
+ * Time: 1:34 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ExportExtension
+{
+	/// <summary>
+	/// Description of EnumSource.
+	/// </summary>
+	public enum EnumSource
+	{
+		Character,
+		IBM
+	}
+}
