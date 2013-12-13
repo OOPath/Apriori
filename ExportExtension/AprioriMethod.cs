212456ef8dc427f35e60cee55a189e17d6d270d9
Seaw.Jean.Aw <Twitter: @OOPath>
2013-12-13 12:42:24 +0700

    fix bug

---
+++ ExportExtension/AprioriMethod.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: OOPath
+ * Date: 12/8/2013
+ * Time: 1:42 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExportExtension.Model;
+
+namespace ExportExtension
+{
+	/// <summary>
+	/// Description of AprioriMethod.
+	/// </summary>
+	public static partial class AprioriExtension
+	{
+		/// <summary>
+		/// TransID     ItemID : Data
+		/// 1           7 127
+		/// 2           0 52
+		/// </summary>
+		/// <param name="items"></param>
+		/// <param name="minimumSupport"></param>
+		/// <returns></returns>
+		private static IList<Item> GetIBMFirstItemSet(IList<Item> items, int minimumSupport)
+		{
+			List<Item> itemSet;
+			List<string> itemIDs = new List<string>();
+			
+			items.AsParallel().ForAll(x => { 
+			                          	string[] str = x.Data.Split(' ');
+			                          	itemIDs.AddRange(str);
+			                          });
+			try {
+				var freqItems = 
+				from c in itemIDs.AsParallel()
+				group c by c into g
+				let transSupport = g.Count()
+				let data = g.Key
+				where transSupport >= minimumSupport
+				orderby data.Length, data
+				select new Item {
+					Data = data,
+					MinimumSupport = minimumSupport, 
+					TransactionSupport = transSupport};
+			
+				itemSet = freqItems.ToList();
+			} catch (Exception) {
+				itemSet = new List<Item>();
+			}
+			
+			return itemSet;
+		}
+		
+		private static IList<Item> GetCharFirstItemSet(IList<Item> items)
+		{
+			List<Item> itemSet = new List<Item>();
+			
+			int minimumSupport = Convert.ToInt32(items.First().Data);
+			string EOF = items.Last().Data;
+//			Console.WriteLine("EOF = {0}", EOF);
+			List<char> chList = new List<char>();
+			foreach (var item in items.Skip(1)) {
+				if (!item.Data.Equals(EOF)) {
+					chList.AddRange(item.Data.ToCharArray());
+				}
+			}
+			var freqItems = 
+				from c in chList
+				group c by c into g
+				let transSupport = g.Count()
+				let data = g.Key.ToString()
+				where transSupport >= minimumSupport
+				orderby data
+				select new Item {
+					Data = data,
+					MinimumSupport = minimumSupport, 
+					TransactionSupport = transSupport};
+			
+//			Console.WriteLine("Freq count = {0}", freqItems.Count());
+			
+			itemSet = freqItems.ToList();
+			
+			if (itemSet.Count == 0) {
+				itemSet.Add(new Item{ Data = EOF });
+			}
+			
+			return itemSet;
+		}
+	}
+}
---
+++ ExportExtension/ItemSetFactory.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: OOPath
+ * Date: 12/7/2013
+ * Time: 1:58 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExportExtension.Model;
+
+namespace ExportExtension
+{
+	/// <summary>
+	/// Description of ItemSetGeneration.
+	/// </summary>
+	public class ItemSetFactory
+	{
+		/// <summary>
+		/// Example: 
+		/// 2
+		/// cab
+		/// bac
+		/// __
+		/// </summary>
+		/// <param name="fileLocation"></param>
+		/// <returns>dataSet</returns>
+		public static IList<Item> GetCharDataSet(string fileLocation)
+		{
+			IList<Item> dataSet = new List<Item>();
+			string line = string.Empty;
+			
+			Task.Run(async() => {
+						using(TextReader file = File.OpenText(fileLocation))
+					    {        
+					        while((line = await file.ReadLineAsync()) != null)
+					        {
+					        	dataSet.Add(new Item{Data = line});
+					        }
+					    }
+			         });
+			return dataSet;
+		}
+		
+		/// <summary>
+		/// TranID      ItemID
+		/// 1     1      7
+		/// 1     1      127
+		/// 2     2      0
+		/// 2     2      52
+		/// </summary>
+		/// <param name="fileLocation"></param>
+		/// <returns></returns>
+		public static IList<Item> GetIBMDataSet(string fileLocation)
+		{
+			IList<Item> dataSet = new List<Item>();
+			string line = string.Empty;
+			
+			Task.Run(async() => {
+						using(TextReader file = File.OpenText(fileLocation))
+					    {        
+					        while((line = await file.ReadLineAsync()) != null)
+					        {
+					        	string[] str = line.Trim().Split(' ');
+					        	dataSet.Add(new Item{
+					        	            	TransactionId = str[0],
+					        	            	Data = str[str.Length - 1]
+					        	            });
+					        }
+					    }
+			         });
+			
+			/*
+			 * Transpose to 
+			 * TranID      ItemID
+			 * 1           7 127
+			 * 2           0 52
+			 * 
+			 * */
+			var transpose = from item in dataSet
+				group item by item.TransactionId into g
+				select new {TransID = g.Key, Items = g};
+	
+			IList<Item> itemList = new List<Item>();
+			var list = transpose.ToList();
+			list.AsParallel().ForAll(x => {
+				Item item = new Item();
+				item.TransactionId = x.TransID;
+				foreach (var e in x.Items)
+				{
+					item.Data += e.Data + " ";
+				}
+				itemList.Add(item);
+			});
+							
+			return itemList;
+		}
+	}
+}
